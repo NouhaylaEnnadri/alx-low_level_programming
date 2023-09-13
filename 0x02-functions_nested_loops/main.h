1393f76b90f1316597cb387e629011bb06adf94d
noyl <nouhaylaennadri@gmail.Com>
2023-09-13 15:03:06 -0700

    lax

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,12 +4,17 @@
 *Desc:header file containing functions declaration
 */
 
-int _putchar(char);
 void print_alphabet(void);
+void jack_bauer(void);
 void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int add(int n, int m);
+int print_last_digit(int n);
 
