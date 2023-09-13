6534c4070ce6794937a0ec4e318c298b8ae2e4ce
noyl <nouhaylaennadri@gmail.Com>
2023-09-13 10:57:29 -0700

    alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
-
 int main(void)
 {
+    char letters[10] = "_putchar\n";
+    int i;
 
-       char letters[10] = "_putchar\n"; 
+    for (i = 0; i < 9; i++)
+    {
+        _putchar(letters[i]);
+    }
 
-       int i; 
-
-       for (i = 0 ; i < 9 ; i++)
-       {
-	       _putchar(letters[i]); 
-       }
-      
-       return(0); 
-
+    return (0);
 }
 
