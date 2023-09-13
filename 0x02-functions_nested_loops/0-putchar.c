a5a671d94813bc3a0d8f2e18b1f1b7ebd5c77625
noyl <nouhaylaennadri@gmail.Com>
2023-09-13 12:21:51 -0700

    alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,9 +13,7 @@ int main(void)
 char letters[10] = "_putchar\n";
 int i;
 for (i = 0; i < 9; i++)
-{
 _putchar(letters[i]);
-}
 return (0);
 }
 
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include<stdio.h>
 /**
- * function that prints 10 times the alphabet, 
- * in lowercase, followed by a new line.
- * procedure
- */
+*function that prints 10 times the alphabet, 
+*in lowercase, followed by a new line.
+*desc: two loops to operate
+*Return : no return
+*/
 void print_alphabet_x10(void)
 {
 int i = 0;
-while (i<10)
+while (i < 10)
 {
 int letters;
 for (letters = 'a'; letters <= 'z'; letters++)
